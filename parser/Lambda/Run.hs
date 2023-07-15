a1acae612f337fc9ff99e3f165017fdb9faa6282
ej-shafran <ejshafran@hilma.tech>
2023-07-15 20:26:27 +0300

    Interpert and run lambda programs!

---
+++ parser/Lambda/Run.hs
@@ -0,0 +1,42 @@
+module Lambda.Run (runProgram) where
+
+import Control.Applicative ((<|>))
+import Data.List (find)
+import Lambda.Parse
+import Parser
+
+getFunc :: [Definition] -> Expression -> Maybe Function
+getFunc defs (FunctionExpression f) = Just f
+getFunc defs (NameExpression n) = function <$> find ((== n) . name) defs
+getFunc defs (ApplicationExpression f a) = do
+  f' <- getFunc defs f
+  a' <- getFunc defs a
+  callFunc defs f' a'
+
+replaceName :: Expression -> String -> Expression -> Expression
+replaceName exp target goal = case exp of
+  NameExpression n | n == target -> goal
+  FunctionExpression (Function p b) -> FunctionExpression $ Function p (replaceName b target goal)
+  ApplicationExpression f a -> ApplicationExpression (replaceName f target goal) (replaceName a target goal)
+  n -> n
+
+obfuscate :: Function -> Function
+obfuscate (Function p b) = Function newName (replaceName b p (NameExpression newName))
+  where
+    newName = '_' : p
+
+callFunc :: [Definition] -> Function -> Function -> Maybe Function
+callFunc defs f a = getFunc defs $ replaceName (body f) (param f) (FunctionExpression a)
+
+runExpression :: [Definition] -> Expression -> Maybe Function
+runExpression defs (ApplicationExpression f a) = do
+  f' <- getFunc defs f
+  a' <- getFunc defs a
+  result <- callFunc defs f' a'
+  -- TODO: only do this if there are any name clashes
+  return (obfuscate result)
+runExpression defs exp = getFunc defs exp
+
+runProgram input = do
+  (exp, defs) <- parseProgram input
+  runExpression defs exp
